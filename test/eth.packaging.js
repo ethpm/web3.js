e9c8e73f7a389b6b30ff86dd8cedac6e14bf55ea
Adrian Li <li.adrianmc@gmail.com>
2018-10-01 12:42:48 -0400

    add hello world test and refactor folders

---
+++ test/eth.packaging.js
@@ -0,0 +1,49 @@
+var chai = require('chai');
+var assert = chai.assert;
+var FakeHttpProvider = require('./helpers/FakeHttpProvider');
+var Web3 = require('../packages/web3');
+var sha3 = require('../packages/web3-utils').sha3;
+var asciiToHex = require('../packages/web3-utils').asciiToHex;
+
+describe('packaging', function () {
+    var provider;
+    var web3;
+
+    describe('in normal operation', function () {
+        beforeEach(function () {
+            provider = new FakeHttpProvider();
+            web3 = new Web3(provider);
+
+            provider.injectResult({
+                timestamp: Math.floor(new Date() / 1000) - 60,
+            });
+            provider.injectValidation(function (payload) {
+                assert.equal(payload.jsonrpc, '2.0');
+                assert.equal(payload.method, 'eth_getBlockByNumber');
+                assert.deepEqual(payload.params, ['latest', false]);
+            });
+
+            provider.injectResult(1);
+            provider.injectValidation(function (payload) {
+                assert.equal(payload.jsonrpc, '2.0');
+                assert.equal(payload.method, 'net_version');
+                assert.deepEqual(payload.params, []);
+            });
+
+            provider.injectResult({
+                hash: '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
+                blockNumber: '0x0'
+            });
+
+            provider.injectValidation(function (payload) {
+                assert.equal(payload.jsonrpc, '2.0');
+                assert.equal(payload.method, 'eth_getBlockByNumber');
+                assert.deepEqual(payload.params, ['0x0', false]);
+            });
+        });
+
+        it('should have a hello world function', function () {
+            assert.equal(web3.eth.packaging.hello(), "Hello World");
+        });
+    });
+});
