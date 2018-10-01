bc49905da7bc37b43f4e3c95c47e2ca364a2ec39
Adrian Li <li.adrianmc@gmail.com>
2018-10-01 11:50:42 -0400

    add skeleton files for new package

---
+++ packages/web3-packaging/src/index.js
@@ -0,0 +1,31 @@
+/*
+    This file is part of web3.js.
+
+    web3.js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    web3.js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file index.js
+ * @author Adrian Li <adrian@trufflesuite.com>
+ * @date 2018
+ */
+
+// var core = require('web3-core');
+// var Subscriptions = require('web3-core-subscriptions').subscriptions;
+// var Method = require('web3-core-method');
+// var formatters = require('web3-core-helpers').formatters;
+// var Net = require('web3-net');
+
+module.exports = {
+    // TODO - fill with accessible API
+};
